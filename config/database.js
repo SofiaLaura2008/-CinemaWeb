6b42c049e1c2a8baa5ff475663cc997ee5e6701b
SofiaLaura2008 <160143058+SofiaLaura2008@users.noreply.github.com>
2025-02-05 09:12:25 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./cinema.sqlite"
+  storage: "./data/cinema.sqlite"
 });
 
 module.exports = sequelize;
